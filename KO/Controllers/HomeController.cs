f38b6d3adf7b5f154a84d4a46d8ab0d2d027d5f1
karlinskidakarlisky@msn.com <akarlisky@msn.com>
2014-03-09 17:46:26 -0400

    first commit

---
+++ KO/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult ObservableArray()
+        {
+            return View("~/Views/ContentViews/ObservableArray.cshtml");
+        }
+
+        public ActionResult Observables()
+        {
+            return View("~/Views/ContentViews/Observables.cshtml");
+        }
+
+        public ActionResult Utilities()
+        {
+            return View("~/Views/ContentViews/Utilities.cshtml");
+
+        }
+
+    }
+}
