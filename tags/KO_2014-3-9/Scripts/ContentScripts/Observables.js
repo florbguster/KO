fea4c33de097fe59c1b87c1f6a046b664d0b05d0
karlinskidakarlisky@msn.com <akarlisky@msn.com>
2014-03-09 21:18:17 -0400

    tagfor version 1 added

---
+++ tags/KO_2014-3-9/Scripts/ContentScripts/Observables.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../jquery-2.1.0.js" />
+/// <reference path="../jquery-2.1.0-vsdoc.js" />
+/// <reference path="../knockout-3.1.0.js" />
+
+
+
+$(function () {
+    // ON LOAD
+
+
+    var viewModel = {
+        // data
+        typedWord: ko.observable("")
+
+        //behaviours
+
+    };
+
+
+    // EVENTS
+    $('#AppendedResultSection').on('mouseenter', 'text', function (event) {
+        var itemToRemove = ko.dataFor(this);
+        itemToRemove.css("color", "Red");
+    });
+
+    ko.applyBindings(viewModel);
+});
